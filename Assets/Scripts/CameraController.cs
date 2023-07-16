0cf2044fee89dde6fd1080d244a398ad7605b956
Jerri Ahonen <jerri.ahonen@gmail.com>
2023-07-16 12:34:05 +0300

    Simplify camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,20 +6,10 @@ public class CameraController : MonoBehaviour
 {
 	[SerializeField] private Vector3 offset;
 	[SerializeField] private float movementSpeed = 3f;
-	[SerializeField] private float zoomSpeed;
-	[SerializeField] private float minFov;
-	[SerializeField] private float maxFov;
-	[SerializeField] private float zoomLimiter = 50f;
 
 	private readonly List<Transform> players = new();
-	private Camera cam;
 	private Bounds bounds = new();
-	private float zoom;
 
-	private void Awake()
-	{
-		cam = GetComponent<Camera>();
-	}
 
 	public void AddPlayer(Transform player) => players.Add(player);
 
@@ -35,8 +25,5 @@ public class CameraController : MonoBehaviour
 			bounds.Encapsulate(t.position);
 
 		transform.position = Vector3.Lerp(transform.position, bounds.center + offset, movementSpeed * Time.deltaTime);
-		float newZoom = Mathf.Lerp(minFov, maxFov, bounds.extents.z / zoomLimiter);
-		zoom = Mathf.Lerp(zoom, newZoom, zoomSpeed * Time.deltaTime);
-		cam.fieldOfView = zoom;
 	}
 }
